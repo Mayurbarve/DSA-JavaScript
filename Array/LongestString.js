04a93950f58a541e4afe36c0131ebd6e7b2bc4de
Mayura Dudhbarve <mayurbarve04@gmail.com>
2024-05-13 21:47:47 +0530

    Longest String in Array

---
+++ Array/LongestString.js
@@ -0,0 +1,15 @@
+function FindLargestString(stringArray) {
+    let longestString = "";
+
+    for (let i = 0; i < stringArray.length; i++) {
+        const currentString = stringArray[i];
+
+        if (currentString.length > longestString.length) {
+            longestString = currentString;
+        }
+    }
+    console.log(longestString);
+}
+
+let myString = ['apple', 'banana', 'pineapple'];
+FindLargestString(myString)
